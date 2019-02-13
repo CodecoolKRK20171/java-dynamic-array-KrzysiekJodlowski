c85581aa6faa9b17f5694663c87cbdfc3a2269d5
KrzysiekJodlowski <krzysztof.s.jodlowski@gmail.com>
2019-02-13 16:49:11 +0100

    Implement insert() method

--- src/main/java/com/codecool/dynamicArrayDojo/DynamicIntArray.java
+++ src/main/java/com/codecool/dynamicArrayDojo/DynamicIntArray.java
@@ -19,32 +19,34 @@ public class DynamicIntArray {
         this.isEmpty = true;
     }
 
-    public void add(int number) {
+    public void add(int value) {
         if (this.internalArraySize == 1 && isEmpty) {
-            this.internalArray[0] = number;
+            this.internalArray[0] = value;
             this.isEmpty = false;
         } else {
             int[] expandedInternalArray = new int[this.internalArraySize + 1];
             for (int index = 0; index < this.internalArraySize; index++) {
                 expandedInternalArray[index] = this.internalArray[index];
             }
-            expandedInternalArray[this.internalArraySize] = number;
+            expandedInternalArray[this.internalArraySize] = value;
             this.internalArray = expandedInternalArray;
             this.internalArraySize++;
         }
-
     }
 
-    public void remove(int numberIndex) {
-        if (this.internalArraySize == 1 && numberIndex == 0) {
+    public void remove(int valueIndex) {
+        if (valueIndex >= this.internalArraySize) {
+            throw new ArrayIndexOutOfBoundsException("No such index in an array!");
+        }
+        if (this.internalArraySize == 1 && valueIndex == 0) {
             this.internalArray[0] = 0;
             this.isEmpty = true;
         } else {
             int[] reducedInternalArray = new int[this.internalArraySize - 1];
             for (int index = 0; index < this.internalArraySize; index++) {
-                if (index < numberIndex) {
+                if (index < valueIndex) {
                     reducedInternalArray[index] = this.internalArray[index];
-                } else if (index > numberIndex){
+                } else if (index > valueIndex){
                     reducedInternalArray[index - 1] = this.internalArray[index];
                 }
             }
@@ -53,8 +55,25 @@ public class DynamicIntArray {
         }
     }
 
-    public void insert(int index, int value) {
-
+    public void insert(int valueIndex, int value) {
+        if (isEmpty) {
+            this.internalArray[0] = value;
+            this.isEmpty = false;
+        } else if (valueIndex >= this.internalArraySize) {
+            this.add(value);
+        } else {
+            int[] expandedInternalArray = new int[++this.internalArraySize];
+            for (int index = 0; index < this.internalArraySize; index++) {
+                if (index < valueIndex) {
+                    expandedInternalArray[index] = this.internalArray[index];
+                } else if (index == valueIndex) {
+                    expandedInternalArray[index] = value;
+                } else {
+                    expandedInternalArray[index] = this.internalArray[index - 1];
+                }
+            }
+            this.internalArray = expandedInternalArray;
+        }
     }
 
     @Override
