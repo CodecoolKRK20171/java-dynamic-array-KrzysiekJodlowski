73100361d47726189e99d4d8b36694570f26d8af
KrzysiekJodlowski <krzysztof.s.jodlowski@gmail.com>
2019-02-13 13:58:24 +0100

    Implement DynamicIntArray default and custom size constructors

--- src/main/java/com/codecool/dynamicArrayDojo/DynamicIntArray.java
+++ src/main/java/com/codecool/dynamicArrayDojo/DynamicIntArray.java
@@ -2,5 +2,15 @@ package com.codecool.dynamicArrayDojo;
 
 // put your code here!
 public class DynamicIntArray {
+    
+    private int[] internalArray;
+
+    public DynamicIntArray() {
+        this.internalArray = new int[10];
+    }
+
+    public DynamicIntArray(int initialSize) {
+        this.internalArray = new int[initialSize];
+    }
 
 }
